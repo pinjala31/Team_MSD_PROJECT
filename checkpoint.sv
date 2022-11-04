726465bc61b7b07a2b14fe2e19c4ec5030b658ea
RaghavaBathula <56624478+RaghavaBathula@users.noreply.github.com>
2022-11-04 16:26:36 -0700

    Update checkpoint.sv

--- checkpoint.sv
+++ checkpoint.sv
@@ -1,5 +1,5 @@
 module command_parser_tb;
-int fd; //to hold the file descriptor
+int fd; //handle to hold the file descriptor
 string line;//string or line read from the file
 string cmd;
 string addr;
@@ -8,13 +8,13 @@ initial
 
 	fd=$fopen("./hex.txt","r"); //opening a file called hex.txt in read mode
 	if(fd) $display("File opened succesfully :%0d",fd);
-	else $display("File was NOT opened :%0d",fd);
+	else $display("File NOT opened :%0d",fd);
 	
 	//to read all lines in the file 
     while(!$feof(fd))begin
 		$fgets(line,fd);
-		cmd=line.substr(0,1);
-		addr=line.substr(2,line.len()-1);
+	    cmd=line.substr(0,1); //created a substring for command
+	    addr=line.substr(2,line.len()-1); //created a subtring for address alone
 		$display("%d is the command", cmd.atoi());
 		$display("%h is the address", addr.atohex());
 		end
